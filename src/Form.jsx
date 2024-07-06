a1aab403cc40e3d4cc782b884d949ae4f4bd01b8
louis <louisbart3@gmail.com>
2024-07-06 23:53:28 +0200

    state fix

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,16 +1,21 @@
 import React from "react";
 import { useState, useEffect } from "react";
 
-export default function Form({ todos, setTodos }) {
-  const [noteData, setNoteData] = useState();
+export default function Form() {
+  //   const [noteData, setNoteData] = useState();
+  //const ID = 0;
+  const [todos, setTodos] = useState([]);
+  const [todoData, setTodoData] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
     let newTodo = {
-      id: todos.length + 1,
-      data: e.currentTarget[0].value,
+      id: 1,
+      data: todoData,
     };
+
+    setTodoData("");
 
     setTodos([...todos, newTodo]);
   };
@@ -24,10 +29,11 @@ export default function Form({ todos, setTodos }) {
       <input
         type="text"
         placeholder="Text"
-        //onChange={(e) => setNoteData(e.currentTarget.value)}
+        value={todoData}
+        onChange={(e) => setTodoData(e.currentTarget.value)}
       />
       <button>Submit</button>
-      {noteData}
+      {/* {noteData} */}
     </form>
   );
 }
