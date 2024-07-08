d721ee018e073cb62b9f4204fc776b052ea6fef4
louis <louisbart3@gmail.com>
2024-07-08 23:31:28 +0200

    fixed unique id generator

--- src/Form.jsx
+++ src/Form.jsx
@@ -4,11 +4,52 @@ import { useState, useEffect } from "react";
 export default function Form({ todos, setTodos }) {
   const [todoData, setTodoData] = useState("");
 
+  const checkUniqueNumber = (newID) => {
+    // condition for generating id
+    console.log(newID);
+    console.log(todos);
+    if (todos.length === 0) return true;
+
+    let unique = true;
+
+    todos.forEach((todo) => {
+      // should be for loop
+      //console.log(`Todo.id: ${todo.id} newID: ${newID}`);
+      if (todo.id === newID) {
+        console.log("Matched should return false");
+        unique = false;
+        return;
+        //return false; // not leaving the function ??
+      }
+    });
+    //console.log("Returning true...");
+
+    return unique;
+    //return true;
+  };
+
+  const generateID = () => {
+    let newID = Math.floor(Math.random() * 100000);
+    ///let newID = 1;
+
+    if (checkUniqueNumber(newID) == false) {
+      // not working
+      console.log("Not unique");
+      generateID();
+    } else {
+      console.log("Unique");
+    }
+
+    return newID;
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
 
+    let newID = generateID();
+
     let newTodo = {
-      id: todos.length + 1, // temporary
+      id: newID, // temporary
       data: todoData,
       completed: false,
     };
